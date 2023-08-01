f00f08abe0656a7e9633bc082fb7ad4349ef26bd
Christopher <illeww@hotmail.com>
2023-08-01 11:43:08 -0400

    Pushing Let's talk app to github

---
+++ Server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const PORT = 3131;
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('Join Room', (data) => {
+    socket.join(data);
+    console.log(`User with id: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on('send_message', (data) => {
+    console.log(data);
+    socket.to(data.room).emit('receive_message', data);
+  });
+  socket.on('disconnect', () => {
+    console.log('User Disconnected', socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('Server is listening on port ' + PORT);
+});
